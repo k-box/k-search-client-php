5fe3d7f316d899da09d8aecd740b34948bacb995
Alessio Vertemati <alessio.vertemati@gmail.com>
2014-10-20 16:40:11 +0200

    Added bootstrap file
    Added bootstrap file for requiring all the classes in the correct manner (php 5.2 compliant)

---
+++ bootstrap.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Bootstrap file
+ *
+ * Include this file to load all the Klink Adapter Boilerplate Classes
+ **/
+
+
+// DO NOT EDIT AFTER THIS LINE ---------------------------------------
+
+if(defined('DEBUG')){
+
+	ini_set('display_errors',1);
+	error_reporting(E_ALL);
+
+}
+
+// Limit the differences between php 5.2 constants and php 5.3 new ones
+
+if(!defined('PHP_VERSION')){
+	define('KLINK_PHP_RUNNING_VERSION', phpversion());
+}
+else {
+	define('KLINK_PHP_RUNNING_VERSION', PHP_VERSION);
+}
+
+// echo 'Klink is running on ' . KLINK_PHP_RUNNING_VERSION. PHP_EOL;
+
+if(strpos(KLINK_PHP_RUNNING_VERSION, '5.2') !== false){
+	define( 'KLINK_COMPATIBILITY_MODE', true);
+}
+
+if( !defined( '__DIR__' ) ) {
+	define( '__DIR__' , dirname(__FILE__ ) . '/vendor' );
+}
+
+// use a plain old loader for php 5.2 while use composer loader for the new PHP versions
+
+if(defined('KLINK_COMPATIBILITY_MODE') && KLINK_COMPATIBILITY_MODE === true){
+	require_once('vendor/php52_autoload.php');
+}
+else {
+	require_once('vendor/autoload.php');
+}
