b2b8d2892d8cf797bc13f84aed184879097d9272
Alessio Vertemati <alessio.vertemati@gmail.com>
2014-11-12 20:11:01 +0100

    documentation on the php5.2 autoload

--- vendor/php52_autoload.php
+++ vendor/php52_autoload.php
@@ -1,35 +1,9 @@
 <?php
 
-//JSON_MAPPER
-
-
-// $map = require __DIR__ . '/composer/autoload_namespaces.php';
-
-// if($map && isset($map[''])){
-
-// 	require_once( __DIR__ . '/avvertix/jsonmapper/src/JsonMapper/Exception.php' );
-// 	require_once( __DIR__ . '/avvertix/jsonmapper/src/JsonMapper.php' );
-
-// 	// $jsonMapper = $map[''];
-
-// 	// var_dump($jsonMapper);
-// }
-
-// foreach ($map as $namespace => $path) {
-//     //require_once( $path );
-    
-// 	echo $namespace . PHP_EOL;
-//     print_r($path);
-    
-// }
-
-// echo 'PSR-4' . PHP_EOL;
-// $map = require __DIR__ . '/composer/autoload_psr4.php';
-// foreach ($map as $namespace => $path) {
-//     //require_once( $path );
-//     print_r($path);
-//     echo $path . PHP_EOL;
-// }
+/**
+ * Autoload for PHP 5.2, based on the latest autoloader created by composer
+ * @package Klink
+ */
 
 
 $classMap = require __DIR__ . '/composer/autoload_classmap.php';
