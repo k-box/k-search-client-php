dc18aa1dbcbe3b5f0b2962cf52a4808690805d66
alessio <alessio.vertemati@gmail.com>
2015-10-05 11:05:24 +0200

    version bump to 0.3.33

--- klink/KlinkCoreClient.php
+++ klink/KlinkCoreClient.php
@@ -5,7 +5,7 @@ if( !defined( 'KLINKADAPTER_DEBUG' ) ){
 }
 
 if( !defined( 'KLINK_BOILERPLATE_VERSION' ) ){
-	define( 'KLINK_BOILERPLATE_VERSION', '0.3.32' );
+	define( 'KLINK_BOILERPLATE_VERSION', '0.3.33' );
 }
 
 /**
@@ -1124,7 +1124,7 @@ final class KlinkCoreClient
 	 * @return string The boilerplate version number.
 	 */
 	public static function version(){
-		return defined('KLINK_BOILERPLATE_VERSION') ? KLINK_BOILERPLATE_VERSION : '0.3.32';
+		return defined('KLINK_BOILERPLATE_VERSION') ? KLINK_BOILERPLATE_VERSION : '0.3.33';
 	}
 
 	// ----- Private Stuff
