d5fa9361204372d8dd4de76cbf447400ca54c6a9
alessio <alessio.vertemati@gmail.com>
2016-04-12 21:04:13 +0200

    added the class JsonStreamEncoder and a basic test to see if something works

---
+++ klink/utils/JsonStreamEncoder.php
@@ -0,0 +1,194 @@
+<?php
+
+
+
+final class JsonStreamEncoder
+{
+	private $_stream;
+
+	/**
+	 * @param resource $stream A stream resource.
+	 * @throws \InvalidArgumentException If $stream is not a stream resource.
+	 */
+	public function __construct($stream)
+	{
+		if (!is_resource($stream) || get_resource_type($stream) != 'stream') {
+			throw new \InvalidArgumentException("Resource is not a stream");
+		}
+
+		$this->_stream = $stream;
+	}
+
+	/**
+	 * Encodes a value and writes it to the stream.
+	 *
+	 * @param mixed $value
+	 */
+	public function encode($value)
+	{
+		// null, bool and scalar values
+		if(is_null($value)) {
+			$this->_writeValue('null');
+			return;
+		}
+		elseif ($value === false) {
+			$this->_writeValue('false');
+			return;
+		}
+		elseif ($value === true) {
+			$this->_writeValue('true');
+			return;
+		}
+		elseif (is_scalar($value)) {
+			$this->_encodeScalar($value);
+			return;
+		}
+
+        
+        if( is_resource($value) && @get_resource_type($value) === 'stream' ){
+            // if it is a PHP stream
+            
+            // $fstat = fstat($value);
+            // var_dump(array_slice($fstat, 13));
+            
+            // I have a stream
+            $this->_writeValue('"'. stream_get_contents($value) .'"');
+            
+            fclose($value); // Let's close the original stream when we have done so PHP can garbage collect some garbage
+            
+            return;
+        }
+        else if( @get_resource_type($value) === 'Unknown' ){
+            var_dump('---> WAS A resource');
+            // I have a stream
+            $this->_writeValue('"WAS A RESOURCE, BUT NOW IS CLOSED"');
+            // throw some exception
+            return;
+        }
+		// array of values
+		else if ($this->_isList($value)) {
+			$this->_encodeList($value);
+			return;
+		}
+		// objects and associative arrays
+		else {
+			$this->_encodeObject($value);
+			return;
+		}
+	}
+
+	/**
+	 * Writes a value to the stream.
+	 *
+	 * @param string $value
+	 */
+	private function _writeValue($value)
+	{
+		fwrite($this->_stream, $value);
+	}
+
+	/**
+	 * Encodes a scalar value.
+	 *
+	 * @param mixed $value
+	 */
+	private function _encodeScalar($value)
+	{
+		if (is_float($value)) {
+			// Always use "." for floats.
+			$encodedValue = floatval(str_replace(",", ".", strval($value)));
+		}
+		elseif (is_string($value)) {
+			$encodedValue = $this->_encodeString($value);
+		}
+		else {
+			// otherwise this must be an int
+			$encodedValue = $value;
+		}
+
+		$this->_writeValue($encodedValue);
+	}
+
+	/**
+	 * Encodes a string.
+	 *
+	 * @param $string
+	 * @return string
+	 */
+	private function _encodeString($string)
+	{
+		static $jsonReplaces = array(array("\\", "/", "\n", "\t", "\r", "\b", "\f", '"', "\0"), array('\\\\', '\\/', '\\n', '\\t', '\\r', '\\b', '\\f', '\"', '\u0000'));
+		return '"' . str_replace($jsonReplaces[0], $jsonReplaces[1], $string) . '"';
+	}
+
+	/**
+	 * Checks if a value is a flat list of values (simple array) or a map (assoc. array or object).
+	 *
+	 * @param mixed $value
+	 * @return bool
+	 */
+	private function _isList($value)
+	{
+		// objects that are not explicitly traversable could never have integer keys, therefore they are not a list
+		if (is_object($value) && !($value instanceof \Traversable)) {
+			return false;
+		}
+
+		// check if the array/object has only integer keys.
+		$i = 0;
+		foreach ($value as $key => $element) {
+			if ($key !== $i) {
+				return false;
+			}
+			$i++;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Encodes a list of values.
+	 *
+	 * @param array $list
+	 */
+	private function _encodeList($list)
+	{
+		$this->_writeValue('[');
+
+		foreach ($list as $x => $value) {
+			$this->encode($value);
+
+			if ($x < count($list) - 1) {
+				$this->_writeValue(',');
+			}
+		}
+
+		$this->_writeValue(']');
+	}
+
+	/**
+	 * Encodes an object or associative array.
+	 *
+	 * @param array|object $object
+	 */
+	private function _encodeObject($object)
+	{
+		$this->_writeValue('{');
+
+		$firstIteration = true;
+
+		foreach ($object as $key => $value) {
+            
+			if (!$firstIteration) {
+				$this->_writeValue(',');
+			}
+			$firstIteration = false;
+
+			$this->_encodeScalar((string)$key);
+			$this->_writeValue(':');
+			$this->encode($value);
+		}
+
+		$this->_writeValue('}');
+	}
+}
---
+++ test/JsonStreamEncoderTest.php
@@ -0,0 +1,85 @@
+<?php
+
+
+class JsonStreamEncoderTest extends PHPUnit_Framework_TestCase
+{
+
+
+	public function setUp()
+	{
+	  	date_default_timezone_set('Europe/Rome');
+        ini_set('memory_limit', '-1');
+	}
+
+	
+
+	public function testEncodeWithInMemoryBase64()
+	{
+        $start = memory_get_usage();
+        $arr = [
+            'document' => base64_encode(file_get_contents('C:\Users\Alessio\Downloads\Brochure.pdf'))
+        ];
+		
+        $temp = tmpfile();
+        
+        $encoder = new JsonStreamEncoder($temp);
+        
+        $encoder->encode($arr);
+        
+        fseek($temp, 0);
+        var_dump(fread($temp, 1024));
+        fclose($temp);
+        
+        $end = memory_get_usage();
+        var_dump('start ' . ($start/1024) . ' KB');
+        var_dump('end ' . ($end/1024) . ' KB');
+        var_dump('diff ' . (($end - $start)/1024) . ' KB');
+        var_dump('Peak memory usage ' . (memory_get_peak_usage(true)/1024) . ' KB');
+        
+        
+        // TODO: test if JSON linting says it is valid
+
+	}
+    
+    public function testEncodeFullStream()
+	{
+        $start = memory_get_usage();
+        
+        $filter = 'convert.base64-encode';
+        $file = 'C:\Users\Alessio\Downloads\Brochure.pdf';
+        $h = fopen('php://filter/read=' . $filter . '/resource=' . $file,'r'); 
+        
+        
+        
+        $arr = [
+            'document' => $h
+        ];
+		
+        var_dump($arr);
+        
+        
+        
+        $temp = tmpfile();
+        
+        $encoder = new JsonStreamEncoder($temp);
+        
+        $encoder->encode($arr);
+        
+        fseek($temp, 0);
+        var_dump('Output truncated to 1024 bytes');
+        var_dump(fread($temp, 1024));
+        // var_dump(stream_get_contents($temp)); // Use this on a green console if you want to experience the Matrix
+        fclose($temp);
+        
+        
+        $end = memory_get_usage();
+        var_dump('start ' . ($start/1024) . ' KB');
+        var_dump('end ' . ($end/1024) . ' KB');
+        var_dump('diff ' . (($end - $start)/1024) . ' KB');
+        var_dump('Peak memory usage ' . (memory_get_peak_usage(true)/1024) . ' KB');
+        
+        // TODO: test if JSON linting says it is valid
+
+	}
+
+}
