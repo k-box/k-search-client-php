866d39a2aea1af26a41610b9e804efee739c3f6d
alessio <alessio.vertemati@gmail.com>
2016-04-14 19:50:21 +0200

    get_mime now returns application/octet-stream when the mime type cannot be determined

--- klink/utils/KlinkDocumentUtils.php
+++ klink/utils/KlinkDocumentUtils.php
@@ -351,18 +351,12 @@ class KlinkDocumentUtils
 	 * Get the mime type of the specified file
 	 * 
 	 * @param string $file the path of the file to get the mime type
-	 * 
 	 * @return string|boolean the mime type or false in case of error
-	 * @throws InvalidArgumentException if extension is empty or if getMimeTypeFromExtension cannot find a mime-type for the given extension
+	 * @throws InvalidArgumentException if $file is empty or null
 	 */
 	public static function get_mime($file) {
 
 		KlinkHelpers::is_string_and_not_empty( $file, 'file' );
-
-		$is_url = !!preg_match('%^https?:\/\/.*$%iu', $file);
-
-		$file_exists = @file_exists($file);
-		$not_is_url  = !$is_url;
 
 		// we don't rely anymore to finfo_file function because for some docx created from LibreOffice the
 		// mime type reported is Composite Document File V2 Document, which has totally no-sense
@@ -374,11 +368,8 @@ class KlinkDocumentUtils
             return self::getMimeTypeFromExtension( $extension );
 
         }
-
-        throw new InvalidArgumentException('Cannot get mime type from file ' . $file . ': Empty extension.' . 
-            ' [is file: ' . var_export($file_exists, true) . 
-                ', is url: ' . var_export($is_url, true) .
-                ', extension: ' . var_export($extension, true). ']' , 2);
+        
+        return 'application/octet-stream';
 
 	}
 
