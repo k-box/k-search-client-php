7d8a3e562c1ea4f3a78f3baf935fa519ce40efee
alessio <alessio.vertemati@gmail.com>
2016-09-12 22:04:31 +0200

    applied the same fix for base64 encoding of files

--- klink/utils/KlinkDocumentUtils.php
+++ klink/utils/KlinkDocumentUtils.php
@@ -416,7 +416,19 @@ class KlinkDocumentUtils
 		}
 		
 		if(@is_file($value)){
-			return fopen('php://filter/read=convert.base64-encode/resource=' . $value,'r');
+
+			$fp = tmpfile();
+			
+			$src = fopen($value, 'r');
+
+			stream_filter_append($fp, 'convert.base64-encode', STREAM_FILTER_WRITE);
+			stream_copy_to_stream($src, $fp);
+			rewind($fp);
+
+			fclose($src);
+
+			return $fp;
+			
 		}
         
         return fopen('data://text/plain,' . base64_encode($value), 'r');
