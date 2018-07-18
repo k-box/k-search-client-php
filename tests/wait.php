2d990f2132bad3f38adebc70d883f60c5f4e5633
Alessio <alessio.vertemati@gmail.com>
2018-07-18 10:16:12 +0200

    Wait for K-Search to be up and running

---
+++ tests/wait.php
@@ -0,0 +1,39 @@
+<?php
+
+require(__DIR__ . '/../vendor/autoload.php');
+
+use Http\Client\HttpClient;
+use Http\Discovery\HttpClientDiscovery;
+use Http\Discovery\MessageFactoryDiscovery;
+
+$messageFactory = MessageFactoryDiscovery::find();
+$httpClient = HttpClientDiscovery::find();
+
+$route = 'http://127.0.0.1:8080/api/3.4/data.get';
+
+$body = '{"params": {"uuid": "cc1bbc0b-20e8-4e1f-b894-fb067e81c5dd"},"id":"request-3d254173"}';
+
+$request = $messageFactory->createRequest('POST', $route, [], $body);
+
+$start = time();
+
+while (true) {
+    try {
+        $response = $httpClient->sendRequest($request);
+
+        if( $response->getStatusCode() === 200 ){
+            fwrite(STDOUT, 'Docker container started!'.PHP_EOL);
+            exit(0);
+        }
+    } catch (PDOException $exception) {
+        $elapsed = time() - $start;
+
+        if ($elapsed > 30) {
+            fwrite(STDERR, 'Docker container did not start in time...'.PHP_EOL);
+            exit(1);
+        }
+
+        fwrite(STDOUT, 'Waiting for container to start...'.PHP_EOL);
+        sleep(1);
+    }
+}
